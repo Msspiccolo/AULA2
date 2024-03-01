b6cf043211ab474b63d3fb68f6d7598d56e37410
MSSP <122064811+Msspiccolo@users.noreply.github.com>
2024-03-01 20:25:15 -0300

    Add files via upload

---
+++ aula2/bin/ex2.dart
@@ -0,0 +1,13 @@
+import 'dart:io';
+
+void main(List<String> args) {
+  print("Valor A: ");
+  int a = int.parse(stdin.readLineSync().toString());
+  print("Valor B: ");
+  int b = int.parse(stdin.readLineSync().toString());
+
+  int r = a + b;
+
+  print("Resultado: $r");
+
+}
